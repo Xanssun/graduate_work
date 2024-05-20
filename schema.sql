901f6a867a86efd318d0c7291352481cc5b681f9
Olya Leonteva <s-c-h-e-r-a@yandex.ru>
2024-05-20 13:10:14 +0300

    beta version of schema

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS cinema;
+CREATE TABLE IF NOT EXISTS cinema.rooms(
+    id uuid PRIMARY KEY,
+    film_id TEXT NOT NULL,
+    creator_id uuid NOT NULL,
+    created_at timestamp NOT NULL,
+    users uuid [],
+);
+CREATE TABLE IF NOT EXISTS cinema.messages(
+    id uuid PRIMARY KEY,
+    user_id uuid NOT NULL,
+    room_id uuid REFERENCES cinema.rooms(id),
+    message TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS cinema.players(
+    id uuid PRIMARY KEY,
+    room_id uuid REFERENCES cinema.rooms(id),
+    is_active BOOLEAN DEFAULT FALSE,
+    view_progress INT,
+);
